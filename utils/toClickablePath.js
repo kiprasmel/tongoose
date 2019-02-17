597c047bfd4d3e6ec99ff4618f4e4a41e37493c5
Kipras Melnikovas <sarpixo@gmail.com>
2019-02-17 20:59:24 +0200

    Create a `toClickablePath` utility function
    Paths printed to the terminal using this utility were clickable on both
    regular bash and integrated (vscode). Not with Windows' cmd though :D
    
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

---
+++ utils/toClickablePath.js
@@ -0,0 +1,15 @@
+/*
+ * toClickablePath.js
+ *
+ * *self explanatory*
+ *
+ * Copyright (c) 2019 Kipras Melnikovas
+ * MIT Licensed
+ */
+
+// foo/bar\index.js => foo\\bar\\index.js (clickable both in integrated & true prompt)
+function toClickablePath(path) {
+	return path.replace(/[\/\\]/g, "\\\\");
+}
+
+module.exports = toClickablePath;
