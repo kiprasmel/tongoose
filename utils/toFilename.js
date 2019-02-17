5f1d8265c7ba1eb29aed49ad6b73257459681fb7
Kipras Melnikovas <sarpixo@gmail.com>
2019-02-17 20:58:16 +0200

    Create a basic `toFilename` utility
    Converts a file path to file name. Optionally removes the extension
    
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

---
+++ utils/toFilename.js
@@ -0,0 +1,18 @@
+/*
+ * toFilename.js
+ *
+ * *self explanatory*
+ *
+ * Copyright (c) 2019 Kipras Melnikovas
+ * MIT Licensed
+ */
+
+// `foo/bar/baz.ext` => `baz.ext` || `baz`
+
+function toFilename(pathToFileNameWithFilename = "", withExtension = true) {
+	return withExtension
+		? pathToFileNameWithFilename.replace(/.*[\/\\]/g, "")
+		: pathToFileNameWithFilename.replace(/.*[\/\\](\w+)(\.\w+)?/g, "$1");
+}
+
+module.exports = toFilename;
