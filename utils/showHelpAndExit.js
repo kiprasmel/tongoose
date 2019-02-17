14b72c9dd1c13e4645694d347c3fa58f353ae9c1
Kipras Melnikovas <sarpixo@gmail.com>
2019-02-17 21:04:19 +0200

    Create `showHelpAndExit` utility (ft. `yargs`)
    Simple & elegant (I suppose)
    
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

---
+++ utils/showHelpAndExit.js
@@ -0,0 +1,18 @@
+/*
+ * showHelpAndExit.js
+ *
+ * *self explanatory*
+ *
+ * Copyright (c) 2019 Kipras Melnikovas
+ * MIT Licensed
+ */
+
+const yargs = require("yargs");
+
+function showHelpAndExit(errorMsg) {
+	yargs.showHelp();
+	errorMsg && console.log("\n" + errorMsg);
+	process.exit(1);
+}
+
+module.exports = showHelpAndExit;
