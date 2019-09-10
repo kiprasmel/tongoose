6be69732fa17154a2d199287c68c075fd417dbcc
Kipras Melnikovas <kipras@kipras.org>
2019-09-10 22:56:05 +0300

    untracked files on dev: 1c1a520 Merge branch 'hotfix--docs' into dev

---
+++ todo--using-native-imports.index.js
@@ -0,0 +1,18 @@
+// #TESTING
+// const currentDir = process.env.PWD;
+
+// console.log("fullFilePath", fullFilePath);
+
+// const importedModelFile = require(path.resolve(currentDir, fullFilePath));
+
+// if (importedModelFile.schema && importedModelFile.schema.tree) {
+// 	importedModelFile.schema = importedModelFile.schema.tree;
+// }
+
+// console.log(
+// 	"TCL: doTheMagicMongooseSchemaIntoJSONObjectsAndTSTypeDefinitionFilesParsing -> importedModelFile",
+// 	// importedModelFile,
+// 	// importedModelFile,
+// 	"\n",
+// 	importedModelFile.schema
+// );
